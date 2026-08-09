81c0ef8d062d0c355b4be06df26a814facddafe6
WywySenarios <wywyericzhu@gmail.com>
2026-08-09 00:17:07 -0400

    feat: defensive bash_completion sourcing with fallback

--- .bashrc.d/completion/bash_completion.sh
+++ .bashrc.d/completion/bash_completion.sh
@@ -1,1 +1,7 @@
-. /usr/share/bash-completion/bash_completion
+if ! shopt -oq posix; then
+	if [ -f /usr/share/bash-completion/bash_completion ]; then
+		. /usr/share/bash-completion/bash_completion
+	elif [ -f /etc/bash_completion ]; then
+		. /etc/bash_completion
+	fi
+fi
